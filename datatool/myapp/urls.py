bbe5a626e383539e0d7255a9b02fc994f682df0d
Mikkel Ziemer <mzhjensen@gmail.com>
2017-05-10 11:49:09 +0200

    Removed tracking of .idea folder

--- datatool/myapp/urls.py
+++ datatool/myapp/urls.py
@@ -4,5 +4,5 @@ from datatool.myapp.views import list, remove
 
 urlpatterns = [
     url(r'^list/$', list, name='list'),
-    url(r'^remove/(?P<file_name>.+)$', remove, name='remove')
+    url(r'^remove/(?P<file_name>.\S+)$', remove, name='remove')
 ]
