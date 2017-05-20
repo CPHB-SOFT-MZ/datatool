7964004155b3bbf73d92fba95e32d31ed253331b
Mikkel Ziemer <mzhjensen@gmail.com>
2017-05-20 16:11:39 +0200

    Rewrote functions to use queues

--- datatool/myapp/analyzetools/tools.py
+++ datatool/myapp/analyzetools/tools.py
@@ -43,51 +43,85 @@ class Tools:
             q.put(('hist', Histogram(self.csv, values=value_header, title=value_header, plot_width=800, legend=False)))
 
     # Finds the row with minimum value for each value header and returns the value and all requested info headers
-    def minimum_value(self, value_headers, info_headers):
+    def minimum_value(self, q, value_headers, info_headers):
+        print("Generating min value")
         df_return = []
         for value_header in value_headers:
-            df = self.csv[info_headers + [value_header]]
-            df_return.append(df.ix[np.argmin(np.array(self.csv[value_header]))])
-        return df_return
+            res_min = DataContainer()
 
-    def median_value(self, value_headers, info_headers):
+            df = self.csv[info_headers + [value_header]]
+
+            value_pairs = df.ix[np.argmin(np.array(self.csv[value_header]))]
+            res_min.value_header = {value_header: value_pairs[value_header]}
+
+            for info_header in info_headers:
+                res_min.append_info_header(info_header, value_pairs[info_header])
+            df_return.append(res_min)
+        q.put(('amin', df_return))
+
+    def median_value(self, q, value_headers, info_headers):
         df_return = []
         for value_header in value_headers:
+            res_medi = DataContainer()
+
             df = self.csv[info_headers + [value_header]]
+
             values = self.csv[value_header]
             median = np.median(np.array(values))
-            df_return.append(df.ix[np.argwhere(median==values)])
-        return df_return
+            value_pairs = df.ix[np.argwhere(median==values)]
+            res_medi.value_header = {value_header: value_pairs[value_header]}
+            for info_header in info_headers:
+                res_medi.append_info_header(info_header, value_pairs[info_header])
+            df_return.append(res_medi)
+        q.put(('median', df_return))
 
-    def average_value(self, value_headers, info_headers):
+    def average_value(self, q, value_headers, info_headers):
         df_return = []
         for value_header in value_headers:
+            res_avg = DataContainer()
             df = self.csv[info_headers + [value_header]]
             values = self.csv[value_header]
             average = np.average(np.array(values))
-            df_return.append(df.ix[np.argwhere(average==values)])
-        return df_return
+            value_pairs = df.ix[np.argwhere(average==values)]
+            res_avg.value_header = {value_header: value_pairs[value_header]}
 
-    def mean_value(self, value_headers, info_headers):
+            for info_header in info_headers:
+                res_avg.append_info_header(info_header, value_pairs[info_header])
+            df_return.append(res_avg)
+        q.put(('avg', df_return))
+
+    def mean_value(self, q, value_headers, info_headers):
         df_return = []
         for value_header in value_headers:
+            res_mean = DataContainer()
             df = self.csv[info_headers + [value_header]]
             values = self.csv[value_header]
             mean = np.mean(np.array(values))
-            df_return.append(df.ix[np.argwhere(mean==values)])
-        return df_return
+            value_pairs = df.ix[np.argwhere(mean == values)]
+            res_mean.value_header = {value_header: value_pairs[value_header]}
 
-    def sum(self, value_headers, info_headers):
+            for info_header in info_headers:
+                res_mean.append_info_header(info_header, value_pairs[info_header])
+            df_return.append(res_mean)
+        q.put(('mean', df_return))
+
+    def sum(self, q, value_headers):
         values = []
         for value_header in value_headers:
+            res_sum = DataContainer()
             sum = np.sum(np.array(self.csv[value_header]))
-            values.append(sum)
-        return values
+            res_sum.value_header = {value_header: sum}
+            values.append(res_sum)
+        q.put(('sum', values))
 
-    def unique_count(self, value_headers, info_headers):
+    def occurences(self, q, value_headers):
         tuple_list = []
         for value_header in value_headers:
+            res_occ = DataContainer()
             array = np.array(self.csv[value_header])
             info, count = np.unique(array, return_counts=True)
-            tuple_list.append(zip(info, count))
-        return tuple_list
+            res = zip(info, count)
+            for r in res:
+                res_occ.info_headers.update({r[0]: r[1]})
+            tuple_list.append(res_occ)
+        q.put(('occur', tuple_list))
