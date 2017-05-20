467940e48a37f921b30fe67c19be9f5296a2376e
Mikkel Ziemer <mzhjensen@gmail.com>
2017-05-20 15:25:33 +0200

    Refactored Maximum class to have a better name

--- datatool/myapp/analyzetools/tools.py
+++ datatool/myapp/analyzetools/tools.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from bokeh.charts import Histogram, Bar
 
-from datatool.myapp.analyzetools.customObjects import Maximum
+from datatool.myapp.analyzetools.customObjects import DataContainer
 
 
 class Tools:
@@ -18,23 +18,22 @@ class Tools:
         print("Generating max value")
         df_return = []
         for value_header in value_headers:
-            ret_max = Maximum()
+            res_max = DataContainer()
 
             df = self.csv[info_headers + [value_header]]
 
             value_pairs = df.ix[np.argmax(np.array(self.csv[value_header]))]
-            ret_max.value_header = {value_header: value_pairs[value_header]}
+            res_max.value_header = {value_header: value_pairs[value_header]}
 
             for info_header in info_headers:
-                ret_max.append_info_header(info_header, value_pairs[info_header])
-            df_return.append(ret_max)
+                res_max.append_info_header(info_header, value_pairs[info_header])
+            df_return.append(res_max)
         q.put(('amax', df_return))
 
     def bar_chart(self, q, value_header):
         print("Generating bar chart")
         bar = Bar(self.csv, label=value_header, title=value_header, plot_width=800, legend=False)
         q.put(('bar', bar))
-
 
     def histogram(self, q, value_header, label_header=None):
         print("Generating histogram")
